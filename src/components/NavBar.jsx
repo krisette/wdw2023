50c6401450bdf54c3ee3220077f98a70696d47b0
Krisette Odegard <kmodeg@gmail.com>
2023-02-25 23:52:58 -0800

    refactor: changed dark mode icons around

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -148,7 +148,7 @@ export default function NavBar() {
 								</div>
 								<div className="ml-auto flex-shrink-0 flex items-center">
 									<span className="sr-only">Dark or light mode</span>
-									{isDark ? (
+									{!isDark ? (
 										<SunIcon className="h-6 w-6 text-white" aria-hidden="true" />
 									) : (
 										<MoonIcon className="h-6 w-6 text-black" aria-hidden="true" />
